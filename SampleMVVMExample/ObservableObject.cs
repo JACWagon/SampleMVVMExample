dde68a2b2d97975ba489800dc69e7755c6292af7
JACWagon <36316989+JACWagon@users.noreply.github.com>
2018-02-09 18:45:08 -0600

    Add project files.

---
+++ SampleMVVMExample/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVVMExample
+{
+    public class ObservableObject
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnChanged([CallerMemberName]string prop = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ SampleMVVMExample/TestViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleMVVMExample
+{
+    public class TestViewModel : ObservableObject
+    {
+        private ObservableCollection<SharedModel> _List;
+        public ObservableCollection<SharedModel> List
+        {
+            get { return _List; }
+            set { _List = value; OnChanged(); }
+        }
+
+        private ICommand _GetSelectedCheckBoxesCommand;
+        public ICommand GetSelectedCheckBoxesCommand
+        {
+            get
+            {
+                if (_GetSelectedCheckBoxesCommand == null)
+                    _GetSelectedCheckBoxesCommand = new ExecutePath(param => GetSelectedCheckBoxes());
+
+                return _GetSelectedCheckBoxesCommand;
+            }
+        }
+
+        public TestViewModel()
+        {
+            List = new ObservableCollection<SharedModel>()
+            {
+                new SharedModel()
+                {
+                    Name = "A1",
+                    IsSelected = false
+                },
+
+                new SharedModel()
+                {
+                    Name = "A2",
+                    IsSelected = false
+                }
+
+            };
+        }
+
+        public List<string> GetSelectedCheckBoxes()
+        {
+            return List.Where(x => x.IsSelected == true).Select(x => x.Name).ToList();
+        }
+    }
+}
